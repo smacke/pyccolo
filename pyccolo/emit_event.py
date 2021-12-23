0f86490bd77c7c01fe735029ffb18602767db569
Stephen Macke <stephen.macke@gmail.com>
2021-12-23 11:41:25 -0800

    undo check for file filter passes in emit_event

--- pyccolo/emit_event.py
+++ pyccolo/emit_event.py
@@ -32,8 +32,7 @@ def _emit_event(event, node_id, **kwargs):
         frame = sys._getframe().f_back
         try:
             for tracer in _TRACER_STACK:
-                if tracer._file_passes_filter_impl(event, frame.f_code.co_filename):
-                    kwargs['ret'] = tracer._emit_event(event, node_id, frame, **kwargs)
+                kwargs['ret'] = tracer._emit_event(event, node_id, frame, **kwargs)
         finally:
             _allow_event_handling = orig_allow_event_handling
     return kwargs.get('ret', None)
