435d31faef04bc22792e80fac8fe4ccdcb0009a0
Stephen Macke <stephen.macke@gmail.com>
2021-12-27 14:17:05 -0800

    dont change encoding pragma of pyi

--- scripts/blacken.sh
+++ scripts/blacken.sh
@@ -3,8 +3,8 @@
 # ref: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 
-find . \( -name '*.py' -or -name '*.pyi' \) -print0 | xargs -0 sed -i.sedbak 's/coding: future_annotations/coding: utf-8/'
+find . -name '*.py' -print0 | xargs -0 sed -i.sedbak 's/coding: future_annotations/coding: utf-8/'
 find . -type f -name '*.sedbak' -print0 | xargs -0 rm
 black .
-find . \( -name '*.py' -or -name '*.pyi' \) -print0 | xargs -0 sed -i.sedbak 's/coding: utf-8/coding: future_annotations/'
+find . -name '*.py' -print0 | xargs -0 sed -i.sedbak 's/coding: utf-8/coding: future_annotations/'
 find . -type f -name '*.sedbak' -print0 | xargs -0 rm
