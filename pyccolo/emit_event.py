ff6d22451e1d37c671bc4b4e0ab16c3c636e0bdc
Stephen Macke <stephen.macke@gmail.com>
2022-01-01 12:08:44 -0600

    reduce nesting in emit_event.py

--- pyccolo/emit_event.py
+++ pyccolo/emit_event.py
@@ -30,28 +30,30 @@ def allow_reentrant_event_handling():
 def _emit_event(event, node_id, **kwargs):
     global _allow_event_handling
     global _allow_reentrant_event_handling
-    if _allow_event_handling or _allow_reentrant_event_handling:
-        orig_allow_event_handling = _allow_event_handling
-        orig_allow_reentrant_event_handling = _allow_reentrant_event_handling
+    if not _allow_event_handling and not _allow_reentrant_event_handling:
+        return kwargs.get("ret", None)
+    frame = sys._getframe().f_back
+    if frame.f_code.co_filename == __file__:
+        # weird shit happens if we instrument this file, so exclude it.
+        return kwargs.get("ret", None)
+    orig_allow_event_handling = _allow_event_handling
+    orig_allow_reentrant_event_handling = _allow_reentrant_event_handling
+    try:
         is_reentrant = not _allow_event_handling
         _allow_event_handling = False
         _allow_reentrant_event_handling = False
-        frame = sys._getframe().f_back
-        try:
-            for tracer in _TRACER_STACK:
-                if tracer._file_passes_filter_impl(
-                    event, frame.f_code.co_filename, is_reentrant=is_reentrant
-                ):
-                    try:
-                        _allow_reentrant_event_handling = (
-                            orig_allow_reentrant_event_handling
-                        )
-                        kwargs["ret"] = tracer._emit_event(
-                            event, node_id, frame, **kwargs
-                        )
-                    finally:
-                        _allow_reentrant_event_handling = False
-        finally:
-            _allow_event_handling = orig_allow_event_handling
-            _allow_reentrant_event_handling = orig_allow_reentrant_event_handling
+        for tracer in _TRACER_STACK:
+            if tracer._file_passes_filter_impl(
+                event, frame.f_code.co_filename, is_reentrant=is_reentrant
+            ):
+                try:
+                    _allow_reentrant_event_handling = (
+                        orig_allow_reentrant_event_handling
+                    )
+                    kwargs["ret"] = tracer._emit_event(event, node_id, frame, **kwargs)
+                finally:
+                    _allow_reentrant_event_handling = False
+    finally:
+        _allow_event_handling = orig_allow_event_handling
+        _allow_reentrant_event_handling = orig_allow_reentrant_event_handling
     return kwargs.get("ret", None)
