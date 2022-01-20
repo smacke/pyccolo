78a776b57c928961e6cad35cc398377351967947
Stephen Macke <stephen.macke@gmail.com>
2022-01-19 22:51:43 -0800

    skip script entrypoint test on py < 3.8

--- test/test_script_entrypoint.py
+++ test/test_script_entrypoint.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
+import sys
 from pyccolo.__main__ import make_parser, run
 
 
-def test_entrypoint_with_script():
-    # just make sure it doesn't raise
-    run(
-        make_parser().parse_args(
-            "./test/uses_null_coalesce.py -t pyccolo.examples.NullCoalescer".split()
+if sys.version_info >= (3, 8):  # noqa
+    def test_entrypoint_with_script():
+        # just make sure it doesn't raise
+        run(
+            make_parser().parse_args(
+                "./test/uses_null_coalesce.py -t pyccolo.examples.NullCoalescer".split()
+            )
         )
-    )
 
 
-def test_entrypoint_with_module():
-    # just make sure it doesn't raise
-    run(
-        make_parser().parse_args(
-            "-m test.uses_null_coalesce -t pyccolo.examples.NullCoalescer".split()
+    def test_entrypoint_with_module():
+        # just make sure it doesn't raise
+        run(
+            make_parser().parse_args(
+                "-m test.uses_null_coalesce -t pyccolo.examples.NullCoalescer".split()
+            )
         )
-    )
