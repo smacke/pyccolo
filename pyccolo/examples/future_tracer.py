e1141ee00ef0a34031d0d7f0b2afb0d22d00b58c
Stephen Macke <stephen.macke@gmail.com>
2022-02-12 12:33:42 -0800

    increase future tracer concurrency

--- pyccolo/examples/future_tracer.py
+++ pyccolo/examples/future_tracer.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 import ast
-import asyncio
 import builtins
 import copy
 import logging
-import threading
-from collections import Counter
-from concurrent.futures import Future
-from typing import Any, Dict, List, Optional, Tuple
+from collections import Counter, defaultdict
+from concurrent.futures import Future, ThreadPoolExecutor
+from typing import Dict, List, Optional, Set, Tuple
 
 import pyccolo as pyc
 import pyccolo.fast as fast
@@ -15,11 +13,6 @@ import pyccolo.fast as fast
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
-
-
-def _run_forever(loop: asyncio.BaseEventLoop):
-    asyncio.set_event_loop(loop)
-    loop.run_forever()
 
 
 _UNWRAP_FUTURE_EXTRA_BUILTIN = "_Xix_PYCCOLO_UNWRAP_FUTURE"
@@ -30,14 +23,26 @@ class FutureUnwrapper(ast.NodeTransformer):
     def __init__(
         self,
         async_vars: Dict[str, int],
+        future_by_name_and_timestamp: Dict[Tuple[str, int], Future],
     ) -> None:
         self._async_vars = async_vars
+        self._future_by_name_and_timestamp = future_by_name_and_timestamp
+        self._deps: List[Future] = []
+
+    def __call__(self, node: ast.AST) -> Tuple[ast.AST, List[Future]]:
+        transformed_node = self.visit(copy.deepcopy(node))
+        deps, self._deps = self._deps, []
+        return ast.Expression(transformed_node), deps
 
     def visit_Name(self, node: ast.Name):
+        assert isinstance(node.ctx, ast.Load)
         current_version = self._async_vars.get(node.id, None)
         if current_version is None:
             return node
         else:
+            self._deps.append(
+                self._future_by_name_and_timestamp[node.id, current_version]
+            )
             with fast.location_of(node):
                 return fast.Call(
                     func=fast.Name(_UNWRAP_FUTURE_EXTRA_BUILTIN, ast.Load()),
@@ -54,45 +59,34 @@ class FutureUnwrapper(ast.NodeTransformer):
                 )
 
 
-async def _fake_coro():
-    return None
-
-
 class FutureTracer(pyc.BaseTracer):
-    _loop = asyncio.new_event_loop()
-    _thread: List[Optional[threading.Thread]] = [None]
+    _MAX_WORKERS = 10
+    _executor: List[Optional[ThreadPoolExecutor]] = [None]
     _async_variable_version_by_name: Dict[str, int] = Counter()
     _future_by_name_and_timestamp: Dict[Tuple[str, int], Future] = {}
-    _value_by_future_id: Dict[int, Any] = {}
-    _empty = object()
+    _waiters_by_future_id: Dict[int, Set[Future]] = defaultdict(set)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         setattr(builtins, _UNWRAP_FUTURE_EXTRA_BUILTIN, self._unwrap_future)
         setattr(builtins, _FUT_TAB_EXTRA_BUILTIN, self._future_by_name_and_timestamp)
-        self._future_unwrapper = FutureUnwrapper(self._async_variable_version_by_name)
-        if self._thread[0] is None:
-            self._thread[0] = threading.Thread(target=_run_forever, args=(self._loop,))
-            self._thread[0].start()
-        self._current_future = asyncio.run_coroutine_threadsafe(
-            _fake_coro(), loop=self._loop
+        self._future_unwrapper = FutureUnwrapper(
+            self._async_variable_version_by_name, self._future_by_name_and_timestamp
         )
+        if self._executor[0] is None:
+            self._executor[0] = ThreadPoolExecutor(max_workers=self._MAX_WORKERS)
 
     @classmethod
     def clear_instance(cls):
         # TODO: replace all created futures with their materialized
         #  values in the stack frames that reference them
-        if cls._loop.is_running():
-            cls._loop.call_soon_threadsafe(cls._loop.stop)
-            cls._thread[0].join()
-            cls._thread[0] = None
+        if cls._executor[0] is not None:
+            cls._executor[0].shutdown()
+            cls._executor[0] = None
         super().clear_instance()
 
     def _unwrap_future(self, fut):
         if isinstance(fut, Future):
-            maybe_val = self._value_by_future_id.get(id(fut), self._empty)
-            if maybe_val is not self._empty:
-                return self._value_by_future_id[id(fut)]
             return fut.result()
         else:
             return fut
@@ -109,25 +103,28 @@ class FutureTracer(pyc.BaseTracer):
         stmt = self.containing_stmt_by_id[id(node)]
         if len(stmt.targets) != 1 or not isinstance(stmt.targets[0], ast.Name):
             return ret
-        unwrap_futures_expr = ast.Expression(
-            self._future_unwrapper.visit(copy.deepcopy(node))
-        )
+        unwrap_futures_expr, deps = self._future_unwrapper(node)
         unwrap_futures_code = compile(unwrap_futures_expr, "<file>", mode="eval")
         async_var = stmt.targets[0].id
         self._async_variable_version_by_name[async_var] += 1
         current_version = self._async_variable_version_by_name[async_var]
 
-        async def async_wrapper():
-            retval = eval(unwrap_futures_code, frame.f_globals, frame.f_locals)
-            self._value_by_future_id[id(fut)] = retval
-            old_fut = self._future_by_name_and_timestamp.pop(
+        def work():
+            old_fut = self._future_by_name_and_timestamp.get(
                 (async_var, current_version - 1), None
             )
-            if old_fut is not None:
-                self._value_by_future_id.pop(id(old_fut), None)
-            return retval
+            for waiter in self._waiters_by_future_id.get(id(old_fut), []):
+                # first, wait on everything that depends on the previous value to finish
+                self._unwrap_future(waiter)
+            # next, garbage collect the previous value
+            self._waiters_by_future_id.pop(id(old_fut), None)
+            self._future_by_name_and_timestamp.pop(
+                (async_var, current_version - 1), None
+            )
+            return eval(unwrap_futures_code, frame.f_globals, frame.f_locals)
 
-        fut = asyncio.run_coroutine_threadsafe(async_wrapper(), loop=self._loop)
-        self._current_future = fut
+        fut = self._executor[0].submit(work)
+        for dep in deps:
+            self._waiters_by_future_id[id(dep)].add(fut)
         self._future_by_name_and_timestamp[async_var, current_version] = fut
         return lambda: fut
