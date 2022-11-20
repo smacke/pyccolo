519b0cba6915e73805c1d9987e825b58d448b7fc
Stephen Macke <stephen.macke@gmail.com>
2022-11-19 18:21:48 -0800

    use syntax extension strategy based on tokenizing

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,87 +1,91 @@
 History
 =======
 
+0.0.39 (2022-11-19)
+-------------------
+* Use Python tokenizer for syntax extensions;
+
 0.0.38 (2022-10-30)
-------------------
+-------------------
 * Better meta_path fallback behavior;
 
 0.0.37 (2022-10-26)
-------------------
+-------------------
 * Bugfix for 3.11;
 
 0.0.36 (2022-10-24)
-------------------
+-------------------
 * Preserve global / nonlocal declarations in copy source;
 
 0.0.35 (2022-08-06)
-------------------
+-------------------
 * Record when passed argument is the last one
 
 0.0.34 (2022-07-18)
-------------------
+-------------------
 * Add enable / disable non-context convenience classmethods;
 
 0.0.33 (2022-07-12)
-------------------
+-------------------
 * Allow before_import handler to overwrite source_path;
 * Allow tracer classes to themselves be used as context managers;
 
 0.0.32 (2022-07-02)
-------------------
+-------------------
 * Disable tracing in import_hooks where applicable;
 
 0.0.31 (2022-07-02)
-------------------
+-------------------
 * Support for before / after import events;
 
 0.0.30 (2022-07-02)
-------------------
+-------------------
 * Better version handling;
 * Add NoopTracer just for use with exec / eval;
 
 0.0.28 (2022-05-30)
-------------------
+-------------------
 * Add 'before_argument' event;
 * OptionalChainer improvements;
 
 0.0.27 (2022-05-30)
-------------------
+-------------------
 * NullCoalescer -> OptionalChainer;
 
 0.0.26 (2022-05-21)
-------------------
+-------------------
 * Get rid of phantom dependency on pytest;
 
 0.0.25 (2022-04-18)
-------------------
+-------------------
 * Allow prefix / suffix augmentations for importfrom statements;
 
 0.0.24 (2022-04-18)
-------------------
+-------------------
 * Allow prefix / suffix augmentations for import statements;
 
 0.0.23 (2022-03-18)
-------------------
+-------------------
 * Support configuring whether global guards enabled;
 * Lazy importer: support unwrapping lazy symbols that result from subscripts;
 
 0.0.22 (2022-03-17)
-------------------
+-------------------
 * Preserve docstring in function definitions;
 * Perform __future__ imports first;
 * Add local guard functionality;
 * Add lazy import example;
 
 0.0.21 (2022-03-02)
-------------------
+-------------------
 * Bugfixes and improvements to FutureTracer example;
 
 0.0.20 (2022-02-14)
-------------------
+-------------------
 * Provide non-context manager variants of tracing-related contexts;
 
 0.0.19 (2022-02-14)
-------------------
+-------------------
 * Add 'exit_module' event;
 * Use deferred evaluation variants for all 'before expr' events;
 * Improve AST bookkeeping;
@@ -89,32 +93,32 @@ History
 * Fix bug where starred expressions weren't traced if used as literal elements;
 
 0.0.17 (2022-02-03)
-------------------
+-------------------
 * Fix packaging issue after new configuration;
 
 0.0.14 (2022-02-02)
-------------------
+-------------------
 * Move configuration out of setup.py;
 
 0.0.13 (2022-01-31)
-------------------
+-------------------
 * Default to all tracers in stack for package-level tracing enabled / disabled context managers;
 * Omit instrumentating the AST of statements underneath "with pyc.tracing_disabled()" bocks;
 * Add SkipAll return value;
 * Improve reentrancy for sys events;
 
 0.0.12 (2022-01-30)
-------------------
+-------------------
 * Expose logic for resolving tracer class based on module path;
 
 0.0.11 (2022-01-30)
-------------------
+-------------------
 * Expanded predicate functionality;
 * New events for after if / while test, after expr stmts, after lambda body, before / after augassign rhs;
 * Disambiguate between user and generated lambdas (e.g. used for before expr events);
 
 0.0.10 (2022-01-26)
-------------------
+-------------------
 * Simplify binop events;
 * Add compare events;
 
--- pyccolo/syntax_augmentation.py
+++ pyccolo/syntax_augmentation.py
@@ -1,30 +1,17 @@
 # -*- coding: utf-8 -*-
-# flake8: noqa
-import ast
-import re
-import sys
+import itertools
 from collections import Counter, defaultdict
 from enum import Enum
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Callable,
-    Dict,
-    List,
-    NamedTuple,
-    Set,
-    Tuple,
-    Union,
-)
+from io import StringIO
+from tokenize import TokenInfo
+from typing import TYPE_CHECKING, Callable, Dict, List, NamedTuple, Set, Tuple, Union
+
+from IPython.core.inputtransformer2 import make_tokens_by_line
 
 if TYPE_CHECKING:
     from pyccolo.ast_rewriter import AstRewriter
 
     CodeType = Union[str, List[str]]
-    if sys.version_info >= (3, 8):
-        Pattern = re.Pattern
-    else:
-        Pattern = Any
 
 
 class AugmentationType(Enum):
@@ -38,58 +25,6 @@ class AugmentationSpec(NamedTuple):
     aug_type: AugmentationType
     token: str
     replacement: str
-
-    @property
-    def escaped_token(self):
-        return re.escape(self.token)
-
-
-# TODO: figure out how to use rulex template
-# https://rulex-rs.github.io/playground/
-
-# let any = [s S];
-# let v = "'";
-# let w = '"';
-# let vvv = "'''";
-# let www = '"""';
-# <%
-# (
-#   (
-#     (!>>v)
-#     (!>>w)
-#     (!>>vvv)
-#     (!>>www)
-#     any
-#   )
-#   | v   ( (!>>v)   any)* v
-#   | w   ( (!>>w)   any)* w
-#   | vvv ( (!>>vvv) any)* vvv
-#   | www ( (!>>www) any)* www
-# )* lazy
-# ('{token}')
-# %>
-
-
-AUGMENTED_SYNTAX_REGEX_TEMPLATE = "".join(
-    r"^(?:"
-    r"   (?:"
-    r"      (?!')"
-    r"      (?!{q})"
-    r"      (?!''')"
-    r"      (?!{tq})"
-    r"      {any}"
-    r"   ) "
-    r"   |  {q}[^{q}]*{q}"
-    r"   |  '[^']*'"
-    r"   |  '''(?:(?!'''){any})*'''"
-    r"   |  {tq}(?:(?!{tq}){any})*{tq}"
-    r" )*?"
-    r" ({{token}})".format(
-        q='"',  # quote
-        tq='"""',  # triple quote
-        any=r"[\S\s]",  # match anything (more general than '.') -- space or non-space
-    ).split()
-)
 
 
 def fix_positions(
@@ -123,48 +58,73 @@ def fix_positions(
 
 
 def replace_tokens_and_get_augmented_positions(
-    s: str, spec: AugmentationSpec, regex: "Pattern"
-) -> Tuple[str, List[int]]:
-    portions = []
+    tokenizable: Union[str, List[TokenInfo]], spec: AugmentationSpec
+) -> Tuple[str, List[Tuple[int, int]]]:
+    if isinstance(tokenizable, str):
+        tokens = list(make_tokens_by_line([tokenizable]))[0]
+    else:
+        tokens = tokenizable
+    transformed = StringIO()
+    match = StringIO()
+    cur_match_start = (-1, -1)
+
+    def _flush_match(force: bool = False) -> None:
+        if force or not spec.token.startswith(match.getvalue()):
+            transformed.write(match.getvalue())
+            match.seek(0)
+            match.truncate()
+
+    def _write_match(tok: TokenInfo) -> None:
+        nonlocal cur_match_start
+        if match.getvalue() == "":
+            cur_match_start = tok.start
+        match.write(tok.string)
+
+    idx = 0
+    col_offset = 0
     positions = []
-    pos_offset = 0
-    while True:
-        m = regex.match(s)
-        if m is None:
-            portions.append(s)
-            break
-        start, _ = m.span(1)
-        positions.append(start + pos_offset)
-        portions.append(s[:start])
-        portions.append(spec.replacement)
-        s = s[start + len(spec.token) :]
-        pos_offset += start + len(spec.replacement)
-    return "".join(portions), positions
+    prev = None
+    while idx < len(tokens):
+        cur = tokens[idx]
+        if prev is not None and prev.end[0] == cur.start[0]:
+            match.write(" " * (cur.start[1] - prev.end[1]))
+            _flush_match()
+        else:
+            col_offset = 0
+            _flush_match(force=True)
+            match.write(" " * cur.start[1])
+        _write_match(cur)
+        _flush_match()
+        if spec.token == match.getvalue():
+            positions.append((cur_match_start[0], cur_match_start[1] + col_offset))
+            col_offset += len(spec.replacement) - len(spec.token)
+            transformed.write(spec.replacement)
+            match.seek(0)
+            match.truncate()
+        prev = cur
+        idx += 1
+
+    _flush_match(force=True)
+    return transformed.getvalue(), positions
 
 
 def make_syntax_augmenter(
     rewriter: "AstRewriter", aug_spec: AugmentationSpec
 ) -> "Callable[[CodeType], CodeType]":
-    regex = re.compile(
-        AUGMENTED_SYNTAX_REGEX_TEMPLATE.format(token=aug_spec.escaped_token)
-    )
-
     def _input_transformer(lines: "CodeType") -> "CodeType":
         if isinstance(lines, list):
             code_lines: List[str] = lines
         else:
             code_lines = lines.splitlines(keepends=True)
-        transformed_lines = []
-        for idx, line in enumerate(code_lines):
-            line, positions = replace_tokens_and_get_augmented_positions(
-                line, aug_spec, regex
-            )
-            transformed_lines.append(line)
-            for pos in positions:
-                rewriter.register_augmented_position(aug_spec, idx + 1, pos)
+        tokens = list(itertools.chain(*make_tokens_by_line(code_lines)))
+        transformed, positions = replace_tokens_and_get_augmented_positions(
+            tokens, aug_spec
+        )
+        for pos in positions:
+            rewriter.register_augmented_position(aug_spec, *pos)
         if isinstance(lines, list):
-            return transformed_lines
+            return transformed.splitlines(keepends=True)
         else:
-            return "".join(transformed_lines)
+            return transformed
 
     return _input_transformer
