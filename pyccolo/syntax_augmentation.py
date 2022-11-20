cd13aeab61a134e90bf59f051985c0a820022d33
Stephen Macke <stephen.macke@gmail.com>
2022-11-19 18:34:50 -0800

    get rid of ipython dependency

--- pyccolo/syntax_augmentation.py
+++ pyccolo/syntax_augmentation.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import itertools
+import tokenize
+import warnings
 from collections import Counter, defaultdict
 from enum import Enum
 from io import StringIO
-from tokenize import TokenInfo
 from typing import TYPE_CHECKING, Callable, Dict, List, NamedTuple, Set, Tuple, Union
-
-from IPython.core.inputtransformer2 import make_tokens_by_line
 
 if TYPE_CHECKING:
     from pyccolo.ast_rewriter import AstRewriter
@@ -58,7 +57,7 @@ def fix_positions(
 
 
 def replace_tokens_and_get_augmented_positions(
-    tokenizable: Union[str, List[TokenInfo]], spec: AugmentationSpec
+    tokenizable: Union[str, List[tokenize.TokenInfo]], spec: AugmentationSpec
 ) -> Tuple[str, List[Tuple[int, int]]]:
     if isinstance(tokenizable, str):
         tokens = list(make_tokens_by_line([tokenizable]))[0]
@@ -74,7 +73,7 @@ def replace_tokens_and_get_augmented_positions(
             match.seek(0)
             match.truncate()
 
-    def _write_match(tok: TokenInfo) -> None:
+    def _write_match(tok: tokenize.TokenInfo) -> None:
         nonlocal cur_match_start
         if match.getvalue() == "":
             cur_match_start = tok.start
@@ -106,6 +105,50 @@ def replace_tokens_and_get_augmented_positions(
 
     _flush_match(force=True)
     return transformed.getvalue(), positions
+
+
+# copied from IPython to avoid bringing it in as a dependency
+# fine since it's BSD licensed
+def make_tokens_by_line(lines: List[str]) -> List[List[tokenize.TokenInfo]]:
+    """Tokenize a series of lines and group tokens by line.
+
+    The tokens for a multiline Python string or expression are grouped as one
+    line. All lines except the last lines should keep their line ending ('\\n',
+    '\\r\\n') for this to properly work. Use `.splitlines(keeplineending=True)`
+    for example when passing block of text to this function.
+
+    """
+    # NL tokens are used inside multiline expressions, but also after blank
+    # lines or comments. This is intentional - see https://bugs.python.org/issue17061
+    # We want to group the former case together but split the latter, so we
+    # track parentheses level, similar to the internals of tokenize.
+    NEWLINE, NL = tokenize.NEWLINE, tokenize.NL
+    tokens_by_line: List[List[tokenize.TokenInfo]] = [[]]
+    if len(lines) > 1 and not lines[0].endswith(("\n", "\r", "\r\n", "\x0b", "\x0c")):
+        warnings.warn(
+            "`make_tokens_by_line` received a list of lines which do not have "
+            + "lineending markers ('\\n', '\\r', '\\r\\n', '\\x0b', '\\x0c'), "
+            + "behavior will be unspecified"
+        )
+    parenlev = 0
+    try:
+        for token in tokenize.generate_tokens(iter(lines).__next__):
+            tokens_by_line[-1].append(token)
+            if (token.type == NEWLINE) or ((token.type == NL) and (parenlev <= 0)):
+                tokens_by_line.append([])
+            elif token.string in {"(", "[", "{"}:
+                parenlev += 1
+            elif token.string in {")", "]", "}"}:
+                if parenlev > 0:
+                    parenlev -= 1
+    except tokenize.TokenError:
+        # Input ended in a multiline string or expression. That's OK for us.
+        pass
+
+    if not tokens_by_line[-1]:
+        tokens_by_line.pop()
+
+    return tokens_by_line
 
 
 def make_syntax_augmenter(
