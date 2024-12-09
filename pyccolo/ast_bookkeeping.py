3c22d5eb3e693bac1e6f75a9648774d68cef3fe3
Stephen Macke <stephen.macke@gmail.com>
2024-12-09 08:36:06 -1000

    fix type error

--- pyccolo/ast_bookkeeping.py
+++ pyccolo/ast_bookkeeping.py
@@ -70,7 +70,8 @@ class BookkeepingVisitor(ast.NodeVisitor):
     def generic_visit(self, node: ast.AST):
         if isinstance(node, ast.stmt):
             self._current_containing_stmt = node
-        self.containing_stmt_by_id[id(node)] = self._current_containing_stmt
+        if self._current_containing_stmt is not None:
+            self.containing_stmt_by_id.setdefault(id(node), self._current_containing_stmt)
         self.ast_node_by_id[id(node)] = node
         if isinstance(node, ast.stmt):
             self.stmt_by_lineno[node.lineno] = node
